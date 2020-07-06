99b5ee8b895a216cbab7b6fb07803328ed516723
changicho <changicho29@gmail.com>
2020-07-06 14:52:20 +0900

    refactor: 변경 babel에 따라 require 제거
    ?: common js 대신 import/export 사용
    www 파일의 경우 기존 방식에서 var > const 로만 변환

--- signup/app.js
+++ signup/app.js
@@ -1,8 +1,8 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
