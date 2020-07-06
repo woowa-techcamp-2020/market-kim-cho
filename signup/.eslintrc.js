fe21fef8352e04d100584443ec08548f5c14cbb7
changicho <changicho29@gmail.com>
2020-07-06 14:50:53 +0900

    Feat: 변경 import/export 방식으로 module import
    ?: babel을 변경해 common js에서 import/export 로 변경
    테스트코드를 위한 jest module 추가

--- signup/.eslintrc.js
+++ signup/.eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2020: true,
   },
   extends: ["eslint:recommended", "airbnb"],
   parserOptions: {
     ecmaVersion: 11,
+    sourceType: "module",
   },
   rules: {
     quotes: ["error", "double", { allowTemplateLiterals: true }],
