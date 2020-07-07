df835ce7eee8d05be834862f4ee6b87733069ab3
changicho <changicho29@gmail.com>
2020-07-07 15:28:45 +0900

    feat: Add emailValidator
    ?: email을 검사하기 위해 정규 표현식 사용
    정규 표현식은 검색해 찾은 방식을 사용
    
    issue: #18

---
+++ signup/function/emailValidator.js
@@ -0,0 +1,6 @@
+export default function emailValidator(email) {
+  const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  if (typeof email !== "string") return false;
+
+  return emailRegex.test(email);
+}
