406f23764e908c255b6dc3be8f349674e68969cd
changicho <changicho29@gmail.com>
2020-07-07 10:41:00 +0900

    feat: 설치 ejs module
    ?: view engine을 ejs로 사용하기 위함
    html 파일로 개발을 완료 후 pug를 사용하기 위해 ejs 모듈 설치
    필요없는 pug 파일 삭제

--- signup/app.js
+++ signup/app.js
@@ -9,9 +9,13 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
-// view engine setup
+// view 경로 설정
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+
+// 화면 engine을 ejs로 설정
+// app.set("view engine", "pug");
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -28,14 +32,14 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render("404.html");
 });
 
 module.exports = app;
