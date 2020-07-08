e3465bf4776ae690fc798933783664232d5d153f
Changi Cho <38618187+changicho@users.noreply.github.com>
2020-07-08 17:04:52 +0900

    로그인 페이지 & 세션 기능 추가 (#24)

---
+++ signup/function/session/session.js
@@ -0,0 +1,51 @@
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const adapter = new FileSync("./db/db.json");
+const db = low(adapter);
+
+/**
+ * 로그인 시 session을 저장하는 함수
+ * @param {string} sessionKey
+ * @param {string} id
+ */
+function setSession(sessionKey, id) {
+  if (sessionKey === "" || typeof sessionKey !== "string") return false;
+
+  const now = new Date().getTime();
+  db.get("session").push({ key: sessionKey, id, time: now }).write();
+
+  return true;
+}
+
+/**
+ * sessionKey가 유효한지 검사하는 함수
+ * @param {string} sessionKey
+ */
+function checkSession(sessionKey) {
+  if (sessionKey === "" || typeof sessionKey !== "string") return false;
+
+  const now = new Date().getTime();
+  const session = db.get("session").find({ key: sessionKey }).value();
+
+  if (session.time + 5 * 60 * 1000 < now) {
+    db.get("session").find({ key: sessionKey }).write();
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * sessionKey로 접속한 유저를 찾는 함수
+ * @param {string} sessionKey
+ */
+function getUserBySession(sessionKey) {
+  if (sessionKey === "" || typeof sessionKey !== "string") return undefined;
+
+  const session = db.get("session").find({ key: sessionKey }).value();
+
+  return db.get("users").find({ id: session.id }).value();
+}
+
+export default { setSession, checkSession, getUserBySession };
--- signup/public/javascripts/login.js
+++ signup/public/javascripts/login.js
@@ -1,6 +1,46 @@
-function login() {
-  const id = document.getElementById("user_id").value;
-  const password = document.getElementById("user_pw").value;
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-absolute-path */
+import idValidator from "/validator/idValidator.js";
+import passwordValidator from "/validator/passwordValidator.js";
+
+const loginButton = document.getElementById("btn_login");
+const idSection = document.querySelector("section.id");
+const passwordSection = document.querySelector("section.password");
+const idInput = document.getElementById("user_id");
+const passwordInput = document.getElementById("user_pw");
+const form = document.querySelector("form.login_form");
+
+const idMessage = document.querySelector("section.id .error_message");
+const passwordMessage = document.querySelector(
+  "section.password .error_message"
+);
+
+function showIdError(errMessage) {
+  idSection.classList.add("error");
+  idMessage.innerText = `${errMessage}`;
+}
+
+function showPasswordError(errMessage) {
+  passwordSection.classList.add("error");
+  passwordInput.value = "";
+  passwordMessage.innerText = `${errMessage}`;
+}
+
+function switching(response) {
+  if (response.isSuccess) {
+    window.location.href = "http://localhost:3000/";
+  } else {
+    if (response.data.isIdError) {
+      showIdError("없는 아이디입니다.");
+    }
+    if (response.data.isPasswordError) {
+      showPasswordError("비밀번호가 잘못되었습니다.");
+    }
+  }
+}
+
+function login(id, password) {
   fetch("http://localhost:3000/login", {
     method: "POST",
     body: JSON.stringify({ id, password }),
@@ -10,9 +50,44 @@ function login() {
     },
   })
     .then((res) => res.json())
-    .then((a) => {
-      console.log(a);
+    .then((res) => {
+      switching(res);
     });
 }
 
-document.getElementById("btn_login").addEventListener("click", login);
+function checkInputEmpty() {
+  const id = idInput.value;
+  const password = passwordInput.value;
+
+  let isError = false;
+  if (id === "") {
+    showIdError("아이디를 입력하세요");
+    isError = true;
+  } else if (!idValidator(id)) {
+    showIdError("유효한 아이디를 입력하세요");
+    isError = true;
+  } else {
+    idSection.classList.remove("error");
+  }
+
+  if (password === "") {
+    showPasswordError("비밀번호를 입력하세요");
+    isError = true;
+  } else if (!passwordValidator(password)) {
+    showPasswordError("유효한 비밀번호를 입력하세요");
+    isError = true;
+  } else {
+    passwordSection.classList.remove("error");
+  }
+
+  if (!isError) {
+    login(id, password);
+  }
+}
+
+loginButton.addEventListener("click", checkInputEmpty);
+form.addEventListener("keydown", (evt) => {
+  if (evt.keyCode === 13) {
+    checkInputEmpty();
+  }
+});
