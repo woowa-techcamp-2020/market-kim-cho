67338c2fd36f68299a782c2dc81e473a843f29ed
changicho <changicho29@gmail.com>
2020-07-09 13:05:44 +0900

    fix: Fix safe guard of complete route
    ?: complete 페이지 rendering 시 데이터를 reset 하는 오류 제거
    req.body에서 할당하며 재할당 하는 오류 해결
    
    undefined checking 을 직접 하도록 변경

--- signup/routes/complete.js
+++ signup/routes/complete.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get("/", (req, res) => {
-  const {
-    name = "name error",
-    id = "id error",
-    email = "email error",
-    phone = "phone error",
-  } = req.body;
+  let { name, id, email, phone } = req.body;
+
+  name = !name ? "name error" : name;
+  id = !id ? "id error" : id;
+  email = !email ? "email error" : email;
+  phone = !phone ? "phone error" : phone;
 
   res.render("complete.ejs", {
     name,
