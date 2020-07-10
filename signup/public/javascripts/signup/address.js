a25be46d3f4b21328d24c5ec77513b608afe1571
yuda1124 <yuda1124@naver.com>
2020-07-10 13:08:40 +0900

    chore: 변경 맵 생성방식
    ?: 주소검색 시 팝업이 아니라 모달 내부에 생성되도록 변경

--- signup/public/javascripts/signup/address.js
+++ signup/public/javascripts/signup/address.js
@@ -1,12 +1,16 @@
 const addressButton = document.querySelector("#find_address");
 const inputPostNumber = document.querySelector("#postnumber");
 const inputAddress = document.querySelector("#address");
+const modal = document.querySelector("#modal_map");
+const wrap = document.querySelector("#wrap");
 
 addressButton.addEventListener("click", (evt) => {
+  wrap.classList.add("mapOn");
   new daum.Postcode({
     oncomplete: function (data) {
       inputPostNumber.value = data.zonecode;
       inputAddress.value = data.address;
+      wrap.classList.remove("mapOn");
     },
-  }).open();
+  }).embed(modal);
 });
